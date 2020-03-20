14c2eff097fbf03b310b57fb16fdec0d71d91dd5
Cristian <cristianguzmansuarez@gmail.com>
2020-03-20 09:40:48 -0600

    Se corrigieron problemas de sigvaris por errores de migraciones anteriores

--- database/migrations/2020_03_18_134014_create_datos_fiscales_table.php
+++ database/migrations/2020_03_18_134014_create_datos_fiscales_table.php
@@ -14,23 +14,23 @@ class CreateDatosFiscalesTable extends Migration
     public function up()
     {
         Schema::create('datos_fiscales', function (Blueprint $table) {
-            $table->increments('id');
-            $table->unsignedInteger('paciente_id');
-            $table->string('tipo_persona')->nullable();
-            $table->string('nombre_o_razon_social')->nullable();
-            $table->string('regimen_fiscal')->nullable();
-            $table->string('homoclave')->nullable();
-            $table->string('correo')->nullable();
-            $table->string('rfc')->nullable();
-            $table->string('calle')->nullable();
-            $table->integer('num_ext')->nullable();
-            $table->integer('num_int')->nullable();
-            $table->string('colonia')->nullable();
-            $table->string('ciudad')->nullable();
-            $table->string('alcaldia_o_municipio')->nullable();
-            $table->string('estado')->nullable();
-            $table->string('codigo_postal')->nullable();
-            $table->timestamps();
+            // $table->increments('id');
+            // $table->unsignedInteger('paciente_id');
+            // $table->string('tipo_persona')->nullable();
+            // $table->string('nombre_o_razon_social')->nullable();
+            // $table->string('regimen_fiscal')->nullable();
+            // $table->string('homoclave')->nullable();
+            // $table->string('correo')->nullable();
+            // $table->string('rfc')->nullable();
+            // $table->string('calle')->nullable();
+            // $table->integer('num_ext')->nullable();
+            // $table->integer('num_int')->nullable();
+            // $table->string('colonia')->nullable();
+            // $table->string('ciudad')->nullable();
+            // $table->string('alcaldia_o_municipio')->nullable();
+            // $table->string('estado')->nullable();
+            // $table->string('codigo_postal')->nullable();
+            // $table->timestamps();
 
             // $table->foreign('paciente_id')->references('id')->on('pacientes');
         });
