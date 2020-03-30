7d30241b9fecc1a2facadf45f39e61e07c894147
JUAN NOLASCO <jjuanccarloss_96@hotmail.com>
2020-03-30 15:13:04 -0600

    venta historial

--- resources/views/venta/index_all.blade.php
+++ resources/views/venta/index_all.blade.php
@@ -241,6 +241,7 @@
                     textapp +=`<td> <div class="row"> <div class="col-auto pr-2"> <a href="{{ url('/ventas') }}/`+item.id+`" class="btn btn-primary"><i class="fas fa-eye"></i><strong> Ver</strong></a> </div>   </div></td>`;
                     var  fecha = new Date();
                         fecha=fecha.setDate(Date.parse(item.fecha) + 31);
+                    console.log("Fecha",fecha);
                     if (fecha > new Date()) {
                         textapp +=`<td nowrap><div class="row"><div class="col-auto pr-2"><a href="{{url('ventas/')}}`+item.id+`/damage" class="btn btn-primary"><i class="fas fa-eye"></i><strong> Damage</strong></a></div></div></td>`;
                     }else{
