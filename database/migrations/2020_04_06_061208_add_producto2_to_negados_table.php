8073393267c4533a61d9dafe9dbdb18c0a5e009a
JUAN NOLASCO <jjuanccarloss_96@hotmail.com>
2020-04-06 01:14:08 -0500

    Prodcuto negado

---
+++ database/migrations/2020_04_06_061208_add_producto2_to_negados_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddProducto2ToNegadosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('negados', function (Blueprint $table) {
+            //
+            $table->integer('producto2')->unsigned()->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('negados', function (Blueprint $table) {
+            //
+        });
+    }
+}
