c7ae993f6731c7a5199fa56c9a711601a21d21a7
JUAN NOLASCO <jjuanccarloss_96@hotmail.com>
2020-04-06 10:59:16 -0500

    Prodcuto negado

--- resources/views/negado/create.blade.php
+++ resources/views/negado/create.blade.php
@@ -17,6 +17,83 @@
             
         </div>
         <div class="card-body">
+            {{-- TABLA DE PACIENTES --}}
+            <div class="row mb-4">
+                <div class="col-12">
+                    <div class="card rounded-0">
+                        <div class="card-header rounded-0">
+                            <div class="row">
+                                <div class="col-sm-12 col-md-6">
+                                    <h3>Pacientes</h3>
+                                </div>
+                                <div class="col-sm-12 col-md-6">
+                                    <label>Buscar:<input type="search" id="BuscarPaciente" onkeypress="return event.keyCode!=13">
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card-body rounded-0">
+                            <div class="table-responsive">
+                                <table class="table" id="pacientes">
+                                    <thead>
+                                        <tr>
+                                            <th>RFC</th>
+                                            <th>Nombre</th>
+                                            <th>Apellidos</th>
+                                            <th>Teléfono</th>
+                                            <th>Seleccionar</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {{-- TABLA DE PRODUCTOS --}}
+            <div class="row mb-4">
+                <div class="col-12">
+                    <div class="card rounded-0">
+                        <div class="card-header rounded-0">
+                            <div class="row">
+                                <div class="col-sm-12 col-md-6">
+                                    <h3>Productos</h3>
+                                </div>
+                                <div class="col-sm-12 col-md-6">
+                                    <label>Buscar:<input type="text" id="BuscarProducto" onkeypress="return event.keyCode!=13">
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+
+
+                        <div class="card-body rounded-0">
+                            <div class="table-responsive">
+                                <table class="table" id="productos">
+                                    <thead>
+                                        <tr>
+                                            <th>SKU</th>
+                                            <th>UPC</th>
+                                            <th>swiss ID</th>
+                                            <th>Producto</th>
+                                            <th>Precio</th>
+                                            <th>Precio con iva</th>
+                                            <th>Agregar</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+
+                                    </tbody>
+                                </table>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
             <form role="form" id="form-cliente" method="POST" action="{{route('negado.create')}}" name="form">
                 {{ csrf_field() }}
                 <div class="card-body">
@@ -27,7 +104,9 @@
                         </div>
                         <div class="col-3 form-group">
                             <label id="No_repetido" class="control-label">✱Paciente:</label>
-                            <input type="text" id="paciente_id" name="paciente_id" class="form-control" required="">
+                            <select class="form-control" name="paciente_id" id="paciente_id" required>
+                                <option value="">Buscar..</option>                                
+                            </select>
                         </div>
                         <div class="col-3 form-group">
                             <label for="actual">✱Fecha actual</label>
@@ -67,5 +146,105 @@
         </div>
     </div>
 </div>
+<script type="text/javascript">
+    $(document).ready(function(){
+        $("#BuscarPaciente").on('keyup', function (e) {
+          var keycode = e.keyCode || e.which;
+            if (keycode == 13) {
+                $("#pacientes").dataTable().fnDestroy();
+            //console.log($(this).val());
+            $('#pacientes').DataTable({
+                "ajax":{
+                    type: "POST",
+                    url:"/getPacientes_nombre",
+                    data: {"_token": $("meta[name='csrf-token']").attr("content"),
+                           "nombre" : $(this).val()
+                    }
+                },
+                "searching": false,
+                pageLength : 3,
+                'language':{
+                    "sProcessing":     "Procesando...",
+                    "sLengthMenu":     "Mostrar _MENU_ registros",
+                    "sZeroRecords":    "No se encontraron resultados",
+                    "sEmptyTable":     "Ningún dato disponible en esta tabla",
+                    "sInfo":           "Productos _START_ al _END_ de un total de _TOTAL_ ",
+                    "sInfoEmpty":      "Productos 0 de un total de 0 ",
+                    "sInfoFiltered":   "(filtrado de un total de _MAX_ registros)",
+                    "sInfoPostFix":    "",
+                    "sSearch":         "Buscar:",
 
+                    "sUrl":            "",
+                    "sInfoThousands":  ",",
+                    "sLoadingRecords": "Cargando...",
+                    "oPaginate": {
+                        "sFirst":    "Primero",
+                        "sLast":     "Último",
+                        "sNext":     "Siguiente",
+                        "sPrevious": "Anterior"
+                    },
+                    "oAria": {
+                        "sSortAscending":  ": Activar para ordenar la columna de manera ascendente",
+                        "sSortDescending": ": Activar para ordenar la columna de manera descendente"
+                    }
+                }
+            });
+
+            }
+        });
+        
+        $("#BuscarProducto").on('keyup', function (e) {
+          var keycode = e.keyCode || e.which;
+            if (keycode == 13) {
+                $("#productos").dataTable().fnDestroy();
+                //console.log($(this).val());
+                $('#productos').DataTable({
+                    "ajax":{
+                        type: "POST",
+                        url:"getProductos_nombre",
+                        data: {"_token": $("meta[name='csrf-token']").attr("content"),
+                               "nombre" : $(this).val()
+                        }
+                    },
+                    "searching": false,
+                    pageLength : 3,
+                    'language':{
+                        "sProcessing":     "Procesando...",
+                        "sLengthMenu":     "Mostrar _MENU_ registros",
+                        "sZeroRecords":    "No se encontraron resultados",
+                        "sEmptyTable":     "Ningún dato disponible en esta tabla",
+                        "sInfo":           "Productos _START_ al _END_ de un total de _TOTAL_ ",
+                        "sInfoEmpty":      "Productos 0 de un total de 0 ",
+                        "sInfoFiltered":   "(filtrado de un total de _MAX_ registros)",
+                        "sInfoPostFix":    "",
+                        "sSearch":         "Buscar:",
+
+                        "sUrl":            "",
+                        "sInfoThousands":  ",",
+                        "sLoadingRecords": "Cargando...",
+                        "oPaginate": {
+                            "sFirst":    "Primero",
+                            "sLast":     "Último",
+                            "sNext":     "Siguiente",
+                            "sPrevious": "Anterior"
+                        },
+                        "oAria": {
+                            "sSortAscending":  ": Activar para ordenar la columna de manera ascendente",
+                            "sSortDescending": ": Activar para ordenar la columna de manera descendente"
+                        }
+                    }
+            });
+            }
+        });      
+    });
+  
+    $(document).on('click', '.botonSeleccionCliente', async function(){
+        
+        const paciente_id = $(this).attr('pacienteid');
+        //const doctor_nombre = $(this).attr('nom');
+        $('#paciente_id').append("<option value='"+paciente_id+"' >nombre</option>");
+        $('#paciente_id').val(paciente_id);
+    /* Act on the event */
+    });
+</script>
 @endsection
