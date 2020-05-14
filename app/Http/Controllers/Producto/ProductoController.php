a29b2163c51ef49a94679d0f087bf20b4e69b7d2
JUAN NOLASCO <jjuanccarloss_96@hotmail.com>
2020-05-14 01:12:10 -0500

    añadiendo historial en editar inventario

--- app/Http/Controllers/Producto/ProductoController.php
+++ app/Http/Controllers/Producto/ProductoController.php
@@ -168,7 +168,8 @@ class ProductoController extends Controller
                     }else{
                         $H=HistorialSurtido::create([
                             'producto_id'=>$ProductoActualizar->id,
-                            'numero'=>1
+                            'numero'=>1,
+                            'user_id' => Auth::user()->id
                         ]);
                     }
                 }else{
