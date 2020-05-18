3e68216b801ff9f8ee979f3e415569197382f162
JUAN NOLASCO <jjuanccarloss_96@hotmail.com>
2020-05-18 17:41:13 -0500

    añadir cumpleaños create ventas

--- app/Http/Controllers/Venta/DescuentoController.php
+++ app/Http/Controllers/Venta/DescuentoController.php
@@ -474,8 +474,9 @@ class DescuentoController extends Controller
         if (isset($paciente->nacimiento)) {
             if (\Carbon\Carbon::parse($paciente->nacimiento)->month==\Carbon\Carbon::now()->month) {
                 $sigpesosCumpleaños=300;
-                $ventasCumple=$paciente->ventas->whereDateBetween('fecha',(new Carbon)->subDays(31)->toDateString(),(new Carbon)->now()->toDateString() )->get();
-
+                
+                //$ventasCumple=$paciente->ventas->whereDateBetween('fecha',(new Carbon)->subDays(31)->toDateString(),(new Carbon)->now()->toDateString() )->get();
+                $ventasCumple=$paciente->ventas->whereDate('fecha','>=',(new Carbon)->subDays(31) )->get();
                 foreach ($ventasCumple as $venta) {
                     if ($venta->cumpleDes==1) {
                         $sigpesosCumpleaños=0;
