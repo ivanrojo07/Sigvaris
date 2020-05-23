53cd407590927358186915b3770fbbabe8beb43a
JUAN NOLASCO <jjuanccarloss_96@hotmail.com>
2020-05-22 23:23:46 -0500

    Ventas create formas de pago folios sigpesosventa

--- app/Http/Controllers/Venta/VentaController.php
+++ app/Http/Controllers/Venta/VentaController.php
@@ -66,7 +66,7 @@ class VentaController extends Controller
             'productos' => $productos,
             'folio' => Venta::count() + 1,
             'empleadosFitter' => $empleadosFitter,
-            'folios' => Folio::get()
+            'Folios' => Folio::get()
         ]);
     }
 
@@ -86,7 +86,7 @@ class VentaController extends Controller
             'productos' => $productos,
             'folio' => Venta::count() + 1,
             'empleadosFitter' => $empleadosFitter,
-            'folios' => Folio::get()
+            'Folios' => Folio::get()
         ]);
     }
     /**
