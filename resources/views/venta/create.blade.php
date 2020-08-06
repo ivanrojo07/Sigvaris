88066780aa7dd3c4b561c2accb682c91c0fe1eee
Marco Antonio Mtz <marcoamtz99@gmail.com>
2020-08-06 02:13:17 -0500

    Cambiando el tipo de INPUT a TEXT
    usando TEXT se captura toda la fecha completa, despues si funciona solo se limitaria  de izquierda a derecha.

--- resources/views/venta/create.blade.php
+++ resources/views/venta/create.blade.php
@@ -342,8 +342,8 @@
                                         </div>
                                         <div class="col-4 form-group">
                                             <label for="" class="text-uppercase text-muted">Fecha: </label>
-                                            <input type="date" name="fecha" class="form-control" readonly=""
-                                                value="{{date('Y-m-d')}}" required="">
+                                            <input type="text" name="fecha" class="form-control" readonly=""
+                                                value="{{date('Y-m-d H:i:s')}}" required="">
                                         </div>
                                         <div class="col-4 form-group">
                                             <label for="" class="text-uppercase text-muted">Folio: </label>
