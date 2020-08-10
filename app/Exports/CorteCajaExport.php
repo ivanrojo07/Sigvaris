2ac6754ec9143bc0502d9793ea15df4aff080ebe
Marco Antonio Mtz <marcoamtz99@gmail.com>
2020-08-09 21:32:49 -0500

    Update CorteCajaExport.php

--- app/Exports/CorteCajaExport.php
+++ app/Exports/CorteCajaExport.php
@@ -55,7 +55,7 @@ class CorteCajaExport implements FromCollection, WithHeadings, WithTitle
 
                     "",
 
-                    Factura::where('venta_id',$Venta->id)->exists()? "Si":"No",
+                    Factura::where('1',$Venta->requiere_factura)->exists()? "Si":"No",
                     $Venta->empleado != null ? $Venta->empleado->nombre : "",
                     "",
                     "0",
