39e4492aefe041a92b8ea3506dc244f737807714
Marco Antonio Mtz <marcoamtz99@gmail.com>
2020-08-12 05:23:21 -0500

    Update create.blade.php

--- resources/views/venta/create.blade.php
+++ resources/views/venta/create.blade.php
@@ -343,7 +343,7 @@
                                         <div class="col-4 form-group">
                                             <label for="" class="text-uppercase text-muted">Fecha: </label>
                                             @php
-                                               $date = = new DateTime(); 
+                                               $date =  new DateTime(); 
                                                 $date->modify('-5 hours');
                                                
                                             @endphp
