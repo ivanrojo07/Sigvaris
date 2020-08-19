080edfedd602c7b15256f6c9f292896a8e4ae5ad
Marco Antonio Mtz <marcoamtz99@gmail.com>
2020-08-19 11:20:15 -0500

    Update index.blade.php

--- resources/views/pacientecrm/index.blade.php
+++ resources/views/pacientecrm/index.blade.php
@@ -182,8 +182,8 @@
                     @else
                     @foreach($paciente->crms as $crm)
                              @php
-                                $ventas = $crm->paciente()->ventas();
-                                $UltimaVenta = $ventas->where($paciente->id,$crm->paciente_id)->last();
+                                $ventas = $crm->ventas();
+                            $UltimaVenta =ventas->where($crm->paciente()->id,$crm->paciente_id)->last();
                              @endphp
                     
                            <tr>
