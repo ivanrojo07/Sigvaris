75ed725f3f8d19f1d4252aa8b13b91e72988955b
MarcoAMtz99 <marcoamtz99@gmail.com>
2020-09-07 08:56:46 -0500

    Prueba excel

--- resources/views/exports/devolucion.blade.php
+++ resources/views/exports/devolucion.blade.php
@@ -1,15 +1,28 @@
 <table>
     <thead>
     <tr>
-        <th>id</th>
-        <th>venta</th>
+        <th>Id</th>
+        <th>Venta</th>
+        <th>Monto</th>
+        <th>Cuenta</th>
+        <th>Beneficiario</th>
+        <th>Referencia</th>
+        <th>Clabe</th>
+        <th>Banco</th>
     </tr>
     </thead>
     <tbody>
-    @foreach($users as $user)
+    @foreach($devolucion as $dev)
         <tr>
-            <td>{{ $devolucion->id }}</td>
-            <td>{{ $devolucion->venta_id }}</td>
+            <td>{{ $dev->id }}</td>
+            <td>{{ $dev->venta_id }}</td>
+            <td>{{ $dev->monto }}</td>
+            <td>{{ $dev->cuenta }}</td>
+            <td>{{ $dev->beneficiario }}</td>
+            <td>{{ $dev->referebcia }}</td>
+            <td>{{ $dev->clave }}</td>
+            <td>{{ $dev->banco }}</td>
+            
         </tr>
     @endforeach
     </tbody>
