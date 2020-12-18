d2721ad2465d8262cfcd2dc4db88a60d605fed0f
MarcoAMtz99 <marcoamtz99@gmail.com>
2020-12-18 12:19:15 -0600

    Update TotalVentasExport.php

--- app/Exports/TotalVentasExport.php
+++ app/Exports/TotalVentasExport.php
@@ -27,7 +27,7 @@ class TotalVentasExport implements FromCollection, WithHeadings, WithTitle
         $Devoluciones = DB::table('devoluciones')->where('created_at','>=',$now->format('Y-m-d'))->get();
         $Dev = $Devoluciones->sum('monto')*-1;
         $TotalVentas=$Ventas->count();
-        $VentasIVA= $Ventas->sum('total');
+        $VentasIVA= $Ventas->sum('total')+$Dev;
         $VentasSIVA=$Ventas->sum('subtotal');
         $auxNu=[];
         $auxRe=[];
