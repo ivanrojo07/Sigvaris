d9d938930daf599a2547e5976222ff16f10f10b5
MarcoAMtz99 <marcoamtz99@gmail.com>
2020-12-21 10:41:04 -0600

    Update DevolucionPExport.php

--- app/Exports/DevolucionPExport.php
+++ app/Exports/DevolucionPExport.php
@@ -22,8 +22,10 @@ class DevolucionPExport implements FromCollection, WithHeadings,WithTitle
     public function collection()
     {
         $now = Carbon::now('America/Mexico_City');
-        $Devoluciones = Devolucion::where('created_at','>=',$now->format('Y-m-d'));
+
+        $Devoluciones = Devolucion::where('created_at','>=',$now->format('Y-m-d'))->get();
         dd($Devoluciones->venta_id);
+
         return Devolucion::where('created_at', '>=',$now->format('Y-m-d'))
             ->get()
             //->pluck('productos')
