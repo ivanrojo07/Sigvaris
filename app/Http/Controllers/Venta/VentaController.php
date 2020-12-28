281f980f97e43677dd1333c1fd85ac46762181bb
MarcoAMtz99 <marcoamtz99@gmail.com>
2020-12-28 07:35:54 -0600

    Update VentaController.php

--- app/Http/Controllers/Venta/VentaController.php
+++ app/Http/Controllers/Venta/VentaController.php
@@ -292,7 +292,7 @@ class VentaController extends Controller
              //Sigpesos 
              foreach ($request->folio as $key => $folio) {
                     # code...
-                    dd($request->folio);
+                    
                     $new_fo = DB::table('sigpesosventa')->where('folio',$request->lista[$key])->exists();
                     $existe = DB::table('sigpesosventa')->where('folio',$folio)->exists();
                     if ($existe = true) {
@@ -311,7 +311,9 @@ class VentaController extends Controller
                         'tipo'=>'pago',
                         'usado'=>1
                          ]);
+                        dd($Sigpesos);
                         $Sigpesos->save();
+                       
                         
                     }
                     
