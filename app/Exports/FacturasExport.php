1756796aa03f5a74f52eaefc05a09ff8623c4d71
MarcoAMtz99 <marcoamtz99@gmail.com>
2021-03-25 08:23:28 -0600

    Update FacturasExport.php

--- app/Exports/FacturasExport.php
+++ app/Exports/FacturasExport.php
@@ -38,9 +38,9 @@ class FacturasExport implements FromCollection, WithHeadings
 
         $ventas = Venta::where('requiere_factura', 0);
 
-        if (!is_null($this->fecha)) {
-            $ventas = $ventas->where('fecha', "=", $this->fecha);
-        }
+        // if (!is_null($this->fecha)) {
+        //     $ventas = $ventas->where('fecha', "=", $this->fecha);
+        // }
 
         if (!is_null($this->oficina_id)) {
             $ventas = $ventas->where('oficina_id', $this->oficina_id);
