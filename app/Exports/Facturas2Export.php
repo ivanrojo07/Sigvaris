72b88a7177f04e83f69e7cb95a87d0551389ebfe
MarcoAMtz99 <marcoamtz99@gmail.com>
2021-03-25 08:59:32 -0600

    Update Facturas2Export.php

--- app/Exports/Facturas2Export.php
+++ app/Exports/Facturas2Export.php
@@ -36,10 +36,11 @@ class Facturas2Export implements FromCollection, WithHeadings
     public function collection()
     {
 
-        $ventas = Venta::where('requiere_factura', 1);
-         $limite = $this->fecha." 23:59:59";
+         // $limite = $this->fecha." 23:59:59";
         if (!is_null($this->fecha)) {
-           $ventas = $ventas->where('created_at', ">=", $this->fecha)->where('created_at','<=',$limite);
+           // $ventas = $ventas->where('created_at', ">=", $this->fecha)->where('created_at','<=',$limite);
+         $limite = $this->fecha." 23:59:59";
+        $ventas = Venta::where('requiere_factura', 1)->where('created_at', ">=", $this->fecha)->where('created_at','<=',$limite);
         }
 
         if (!is_null($this->oficina_id)) {
