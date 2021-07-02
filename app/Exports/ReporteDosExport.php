2a0b244a8da968a28d248c36116b298f43b7e560
MarcoAMtz99 <marcoamtz99@gmail.com>
2021-07-02 09:25:59 -0500

    Update ReporteDosExport.php

--- app/Exports/ReporteDosExport.php
+++ app/Exports/ReporteDosExport.php
@@ -7,6 +7,7 @@ use App\Paciente;
 use App\Producto;
 // use Maatwebsite\Excel\Concerns\FromCollection;
 // use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\FromView;
 use Illuminate\Contracts\View\View;
 
 class ReporteDosExport implements FromView
