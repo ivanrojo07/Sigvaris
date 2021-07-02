75436ec75d141f3979ac8ab9b64b4a7571253c22
MarcoAMtz99 <marcoamtz99@gmail.com>
2021-07-02 09:20:54 -0500

    reportedos

--- app/Exports/ReporteDosExport.php
+++ app/Exports/ReporteDosExport.php
@@ -8,9 +8,9 @@ use App\Producto;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 
-class ReporteDosExport implements FromCollection,  WithHeadings
-{
-	public function __construct($fechaInicial,$fechaFinal,$ventas)
+class ReporteDosExport implements FromView
+    {
+        public function __construct($fechaInicial,$fechaFinal,$ventas)
     {
         
          $this->fechaInicial = $fechaInicial;
@@ -18,99 +18,112 @@ class ReporteDosExport implements FromCollection,  WithHeadings
          $this->ventas = $ventas;
         
     }
-
-
-  public function ventasFecha($fechaInicial,$fechaFinal){
-            $ventas = Venta::has('paciente')->has('productos')->where('fecha', '>=', $fechaInicial)
-                ->where('fecha', '<=', $fechaFinal)
-                ->withCount('productos');
-        $ventas = $ventas->get();
-
-        return $ventas;
-    }
-      public function ventas($fechaInicial,$fechaFinal){
-            $ventas = Venta::has('paciente')->has('productos')->where('fecha', '>=', $fechaInicial)
-                ->where('fecha', '<=', $fechaFinal)
-                ->withCount('productos');
-        $ventas = $ventas->get();
-
-        return $ventas->fecha;
-    }
-      public function ventasNombre($fechaInicial,$fechaFinal){
-            $ventas = Venta::has('paciente')->has('productos')->where('fecha', '>=', $fechaInicial)
-                ->where('fecha', '<=', $fechaFinal)
-                ->withCount('productos');
-        $ventas = $ventas->get();
-
-        return $ventas->paciente->nombre;
-    }
-      public function ventasPaterno($fechaInicial,$fechaFinal){
-            $ventas = Venta::has('paciente')->has('productos')->where('fecha', '>=', $fechaInicial)
-                ->where('fecha', '<=', $fechaFinal)
-                ->withCount('productos');
-        $ventas = $ventas->get();
-
-        return $ventas->paciente->paterno;
-    }
-       public function ventasMaterno($fechaInicial,$fechaFinal){
-            $ventas = Venta::has('paciente')->has('productos')->where('fecha', '>=', $fechaInicial)
-                ->where('fecha', '<=', $fechaFinal)
-                ->withCount('productos');
-        $ventas = $ventas->get();
-
-        return $ventas->paciente->materno;
-    }
-      public function ventasCantidad($fechaInicial,$fechaFinal){
-            $ventas = Venta::has('paciente')->has('productos')->where('fecha', '>=', $fechaInicial)
-                ->where('fecha', '<=', $fechaFinal)
-                ->withCount('productos');
-        $ventas = $ventas->get();
-
-        return $ventas->cantidad_productos;
-    }
-
-    /**
-    * @return \Illuminate\Support\Collection
-    */
-    public function collection()
+	    public function view(): View
     {
-        // $ventas = $this->ventas($this->fechaInicial,$this->fechaFinal);
-        // 
-       
-        return collect(
-                 [
-                'fecha'=>$this->ventasFecha($this->fechaInicial,$this->fechaFinal),
-                'nombre'=>$this->ventasNombre($this->fechaInicial,$this->fechaFinal),
-                'paterno'=>$this->ventasPaterno($this->fechaInicial,$this->fechaFinal),
-                'materno'=>$this->ventasMaterno($this->fechaInicial,$this->fechaFinal),
-                'cantidad_productos'=>$this->ventasCantidad($this->fechaInicial,$this->fechaFinal)
-                     ]);
-        
-    }
-    // public function collection()
-    // {
-    //     // $ventas = $this->ventas($this->fechaInicial,$this->fechaFinal);
-    //     // 
-       
-    //     return collect(
-    //     		 [
-    //     		'fecha'=>$this->ventas($this->fechaInicial,$this->fechaFinal)->fecha,
-    //     		'nombre'=>$this->ventas($this->fechaInicial,$this->fechaFinal)->paciente->nombre,
-    //     		'paterno'=>$this->ventas($this->fechaInicial,$this->fechaFinal)->paciente->paterno,
-    //     		'materno'=>$this->ventas($this->fechaInicial,$this->fechaFinal)->paciente->materno,
-    //     		'cantidad_productos'=>$this->ventas->cantidad_productos
-    //     			 ]);
-        
-    // }
-
-    public function headings(): array
-    {
-        return [
-            'fecha',
-            'nombre',
-            'paterno',
-            'materno',
-            'NÚM. prendas'
-        ];
+        $fechaInicial =$this->fechaInicial;
+        $fechaFinal =$this->fechaFinal;
+        $ventas = Venta::has('paciente')->has('productos')->where('fecha', '>=', $fechaInicial)
+                ->where('fecha', '<=', $fechaFinal)
+                ->withCount('productos');
+        $ventas = $ventas->get();
+        return view('exports.reporteDos', [
+            'ventas' =>$ventas
+        ]);
     }
 }
+
+
+
+ //  public function ventasFecha($fechaInicial,$fechaFinal){
+ //            $ventas = Venta::has('paciente')->has('productos')->where('fecha', '>=', $fechaInicial)
+ //                ->where('fecha', '<=', $fechaFinal)
+ //                ->withCount('productos');
+ //        $ventas = $ventas->get();
+
+ //        return $ventas;
+ //    }
+ //      public function ventas($fechaInicial,$fechaFinal){
+ //            $ventas = Venta::has('paciente')->has('productos')->where('fecha', '>=', $fechaInicial)
+ //                ->where('fecha', '<=', $fechaFinal)
+ //                ->withCount('productos');
+ //        $ventas = $ventas->get();
+
+ //        return $ventas->fecha;
+ //    }
+ //      public function ventasNombre($fechaInicial,$fechaFinal){
+ //            $ventas = Venta::has('paciente')->has('productos')->where('fecha', '>=', $fechaInicial)
+ //                ->where('fecha', '<=', $fechaFinal)
+ //                ->withCount('productos');
+ //        $ventas = $ventas->get();
+
+ //        return $ventas->paciente->nombre;
+ //    }
+ //      public function ventasPaterno($fechaInicial,$fechaFinal){
+ //            $ventas = Venta::has('paciente')->has('productos')->where('fecha', '>=', $fechaInicial)
+ //                ->where('fecha', '<=', $fechaFinal)
+ //                ->withCount('productos');
+ //        $ventas = $ventas->get();
+
+ //        return $ventas->paciente->paterno;
+ //    }
+ //       public function ventasMaterno($fechaInicial,$fechaFinal){
+ //            $ventas = Venta::has('paciente')->has('productos')->where('fecha', '>=', $fechaInicial)
+ //                ->where('fecha', '<=', $fechaFinal)
+ //                ->withCount('productos');
+ //        $ventas = $ventas->get();
+
+ //        return $ventas->paciente->materno;
+ //    }
+ //      public function ventasCantidad($fechaInicial,$fechaFinal){
+ //            $ventas = Venta::has('paciente')->has('productos')->where('fecha', '>=', $fechaInicial)
+ //                ->where('fecha', '<=', $fechaFinal)
+ //                ->withCount('productos');
+ //        $ventas = $ventas->get();
+
+ //        return $ventas->cantidad_productos;
+ //    }
+
+ //    /**
+ //    * @return \Illuminate\Support\Collection
+ //    */
+ //    public function collection()
+ //    {
+ //        // $ventas = $this->ventas($this->fechaInicial,$this->fechaFinal);
+ //        // 
+       
+ //        return collect(
+ //                 [
+ //                'fecha'=>$this->ventasFecha($this->fechaInicial,$this->fechaFinal),
+ //                'nombre'=>$this->ventasNombre($this->fechaInicial,$this->fechaFinal),
+ //                'paterno'=>$this->ventasPaterno($this->fechaInicial,$this->fechaFinal),
+ //                'materno'=>$this->ventasMaterno($this->fechaInicial,$this->fechaFinal),
+ //                'cantidad_productos'=>$this->ventasCantidad($this->fechaInicial,$this->fechaFinal)
+ //                     ]);
+        
+ //    }
+ //    // public function collection()
+ //    // {
+ //    //     // $ventas = $this->ventas($this->fechaInicial,$this->fechaFinal);
+ //    //     // 
+       
+ //    //     return collect(
+ //    //            [
+ //    //           'fecha'=>$this->ventas($this->fechaInicial,$this->fechaFinal)->fecha,
+ //    //           'nombre'=>$this->ventas($this->fechaInicial,$this->fechaFinal)->paciente->nombre,
+ //    //           'paterno'=>$this->ventas($this->fechaInicial,$this->fechaFinal)->paciente->paterno,
+ //    //           'materno'=>$this->ventas($this->fechaInicial,$this->fechaFinal)->paciente->materno,
+ //    //           'cantidad_productos'=>$this->ventas->cantidad_productos
+ //    //                ]);
+        
+ //    // }
+
+ //    public function headings(): array
+ //    {
+ //        return [
+ //            'fecha',
+ //            'nombre',
+ //            'paterno',
+ //            'materno',
+ //            'NÚM. prendas'
+ //        ];
+ //    }
---
+++ resources/views/exports/reporteDos.blade.php
@@ -0,0 +1,36 @@
+ <table class="table table-hover table-striped table-bordered" style="margin-bottom: 0;" id="listaEmpleados">
+                <thead>
+                    <tr class="info">
+                        <th>Fecha</th>
+                        {{-- <th>Doctor</th> --}}
+                        <th>Nombre</th>
+                        <th>Apellido paterno</th>
+                        <th>Apellido materno</th>
+                        <th>N° prendas</th>
+                        {{-- <th>Acción</th> --}}
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($ventas as $venta)
+                    <tr>
+                        <td>{{$venta->fecha}}</td>
+                        <td>{{$venta->paciente ? $venta->paciente->nombre : ''}}</td>
+                        <td>{{$venta->paciente ? $venta->paciente->paterno : ''}}</td>
+                        <td>{{$venta->paciente ? $venta->paciente->materno : ''}}</td>
+                        <td>{{$venta->cantidad_productos}}</td>
+                        {{-- <td>{{App\Paciente::find($paciente_id)->doctor()->first()->nombre}}</td> --}}
+                        {{-- <td>{{App\Paciente::find($paciente_id) ? App\Paciente::find($paciente_id)->nombre : ''}}</td>
+                        <td>{{App\Paciente::find($paciente_id) ? App\Paciente::find($paciente_id)->paterno : ''}}</td>
+                        <td>{{App\Paciente::find($paciente_id) ? App\Paciente::find($paciente_id)->materno : ''}}</td>
+                        <td>{{
+                                            $ventas->pluck('productos')
+                                                ->flatten()
+                                                ->pluck('pivot')
+                                                ->flatten()
+                                                ->pluck('cantidad')
+                                                ->sum()
+                                            }}</td> --}}
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
