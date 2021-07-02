3acc2f8d7ea7d314ce33ab3a5b0becc4df8f6a6a
MarcoAMtz99 <marcoamtz99@gmail.com>
2021-07-02 09:05:13 -0500

    Update ReporteDosExport.php

--- app/Exports/ReporteDosExport.php
+++ app/Exports/ReporteDosExport.php
@@ -20,13 +20,53 @@ class ReporteDosExport implements FromCollection,  WithHeadings
     }
 
 
-    public function ventas($fechaInicial,$fechaFinal){
-    		$ventas = Venta::has('paciente')->has('productos')->where('fecha', '>=', $fechaInicial)
+  public function ventasFecha($fechaInicial,$fechaFinal){
+            $ventas = Venta::has('paciente')->has('productos')->where('fecha', '>=', $fechaInicial)
                 ->where('fecha', '<=', $fechaFinal)
                 ->withCount('productos');
         $ventas = $ventas->get();
 
         return $ventas;
+    }
+      public function ventas($fechaInicial,$fechaFinal){
+            $ventas = Venta::has('paciente')->has('productos')->where('fecha', '>=', $fechaInicial)
+                ->where('fecha', '<=', $fechaFinal)
+                ->withCount('productos');
+        $ventas = $ventas->get();
+
+        return $ventas->fecha;
+    }
+      public function ventasNombre($fechaInicial,$fechaFinal){
+            $ventas = Venta::has('paciente')->has('productos')->where('fecha', '>=', $fechaInicial)
+                ->where('fecha', '<=', $fechaFinal)
+                ->withCount('productos');
+        $ventas = $ventas->get();
+
+        return $ventas->paciente->nombre;
+    }
+      public function ventasPaterno($fechaInicial,$fechaFinal){
+            $ventas = Venta::has('paciente')->has('productos')->where('fecha', '>=', $fechaInicial)
+                ->where('fecha', '<=', $fechaFinal)
+                ->withCount('productos');
+        $ventas = $ventas->get();
+
+        return $ventas->paciente->paterno;
+    }
+       public function ventasMaterno($fechaInicial,$fechaFinal){
+            $ventas = Venta::has('paciente')->has('productos')->where('fecha', '>=', $fechaInicial)
+                ->where('fecha', '<=', $fechaFinal)
+                ->withCount('productos');
+        $ventas = $ventas->get();
+
+        return $ventas->paciente->materno;
+    }
+      public function ventasCantidad($fechaInicial,$fechaFinal){
+            $ventas = Venta::has('paciente')->has('productos')->where('fecha', '>=', $fechaInicial)
+                ->where('fecha', '<=', $fechaFinal)
+                ->withCount('productos');
+        $ventas = $ventas->get();
+
+        return $ventas->->cantidad_productos;
     }
 
     /**
@@ -38,15 +78,30 @@ class ReporteDosExport implements FromCollection,  WithHeadings
         // 
        
         return collect(
-        		 [
-        		'fecha'=>$this->ventas($this->fechaInicial,$this->fechaFinal),
-        		'nombre'=>$this->ventas($this->fechaInicial,$this->fechaFinal),
-        		'paterno'=>$this->ventas($this->fechaInicial,$this->fechaFinal),
-        		'materno'=>$this->ventas($this->fechaInicial,$this->fechaFinal),
-        		'cantidad_productos'=>$this->ventas($this->fechaInicial,$this->fechaFinal)
-        			 ]);
+                 [
+                'fecha'=>$this->ventasFecha($this->fechaInicial,$this->fechaFinal),
+                'nombre'=>$this->ventasNombre($this->fechaInicial,$this->fechaFinal),
+                'paterno'=>$this->ventasPaterno($this->fechaInicial,$this->fechaFinal),
+                'materno'=>$this->ventasMaterno($this->fechaInicial,$this->fechaFinal),
+                'cantidad_productos'=>$this->ventasCantidad($this->fechaInicial,$this->fechaFinal)
+                     ]);
         
     }
+    // public function collection()
+    // {
+    //     // $ventas = $this->ventas($this->fechaInicial,$this->fechaFinal);
+    //     // 
+       
+    //     return collect(
+    //     		 [
+    //     		'fecha'=>$this->ventas($this->fechaInicial,$this->fechaFinal)->fecha,
+    //     		'nombre'=>$this->ventas($this->fechaInicial,$this->fechaFinal)->paciente->nombre,
+    //     		'paterno'=>$this->ventas($this->fechaInicial,$this->fechaFinal)->paciente->paterno,
+    //     		'materno'=>$this->ventas($this->fechaInicial,$this->fechaFinal)->paciente->materno,
+    //     		'cantidad_productos'=>$this->ventas->cantidad_productos
+    //     			 ]);
+        
+    // }
 
     public function headings(): array
     {
