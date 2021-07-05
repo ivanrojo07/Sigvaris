98fbb5751b84d249c076aed8e52a6c70ff77fb49
MarcoAMtz99 <marcoamtz99@gmail.com>
2021-07-05 05:51:07 -0500

    Update ReporteController.php

--- app/Http/Controllers/Reporte/ReporteController.php
+++ app/Http/Controllers/Reporte/ReporteController.php
@@ -277,7 +277,7 @@ class ReporteController extends Controller
 
         // dd( $arregloSumaPacientes );
 
-        return view('reportes.tres', compact('arregloFechasConVentas', 'arregloTotalPacientesConUnProducto', 'arregloTotalPacientesConMasDeUnProducto', 'arregloSumaPacientes', 'totalPacientesConMasDeUnaPrenda', 'totalPacientesConUnaPrenda', 'oficinas', 'empleadosFitter','fechaInicial','fechaFinal',  'mesIni ','anioIni', 'mesFin','anioFin'));
+        return view('reportes.tres', compact('arregloFechasConVentas', 'arregloTotalPacientesConUnProducto', 'arregloTotalPacientesConMasDeUnProducto', 'arregloSumaPacientes', 'totalPacientesConMasDeUnaPrenda', 'totalPacientesConUnaPrenda', 'oficinas', 'empleadosFitter','fechaInicial','fechaFinal','mesIni ','anioIni', 'mesFin','anioFin'));
     }
 
     public function cuatroa(Request $request)
